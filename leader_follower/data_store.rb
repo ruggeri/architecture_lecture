a22e33c569fa7429a7b72c5867d93332787d1eb9
Ned Ruggeri <ruggeri@self-loop.com>
2017-06-09 19:45:29 -0700

    start writing leader RequestServer

--- leader_follower/data_store.rb
+++ leader_follower/data_store.rb
@@ -5,20 +5,38 @@ class ReadOnlyException < Exception; end
 
 class DataStore
   def initialize
+    @data = Hash.new(0)
   end
 
   def increment(key)
+    @data[key] += 1
   end
 
   def square(key)
+    @data[key] *= @data[key]
   end
 
   def get(key)
+    return @data[key]
   end
-  
+
   def perform_command(command, key, read_only:)
+    if command == "GET"
+      return get(key)
+    elsif command == "INCREMENT"
+      raise ReadOnlyException if read_only
+      increment(key)
+      return "OK"
+    elsif command == "SQUARE"
+      raise ReadOnlyException if read_only
+      square(key)
+      return "OK"
+    else
+      raise UnknownCommandException
+    end
   end
 
   def get_log_lines(start_position)
+    # TODO: implement with followers.
   end
 end
--- leader_follower/request_server.rb
+++ leader_follower/request_server.rb
@@ -3,17 +3,40 @@ require 'socket'
 
 class RequestServer
   def initialize(data_store, port, read_only:)
+    @data_store = data_store
+    @request_server = TCPServer.new(port)
+    @read_only = read_only
   end
 
   def run!
+    while true
+      connection = @request_server.accept
+      handle_client_connection!(connection)
+    end
   end
 
   def join
   end
 
-  def handle_client_request!(connection)
+  def handle_client_connection!(connection)
+    while true
+      input = connection.gets
+      break if input.nil?
+      command, key = input.chomp.split
+      handle_command(connection, command, key)
+    end
+
+    connection.close
   end
   
   def handle_command(connection, command, key)
+    begin
+      result = @data_store.perform_command(command, key, read_only: @read_only)
+      connection.puts result
+    rescue UnknownCommandException
+      connection.puts "ERROR: UnknownCommandException: #{command}"
+    rescue ReadOnlyException
+      connection.puts "ERROR: Follower server cannot run command: #{command}"
+    end
   end
 end
